a35830b6e92645cdd5a66a4b004b9571be897496
Aristotle Pagalztis <pagaltzis@gmx.de>
2008-09-27 10:58:21 +0200

    improve dialog creation

--- mainwin.c
+++ mainwin.c
@@ -23,29 +23,38 @@ Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include "preview_pane.h"
 #include "about_dialog.h"
 
-static void about_clicked(GtkWidget *widget, gpointer u)
+static void response(GtkWidget *widget, gint response, gpointer u)
 {
-	show_about_dialog();
-}
-
-static void apply_clicked(GtkWidget *widget, gpointer u)
-{
-	apply_new_look(FALSE);
-}
-
-static void ok_clicked(GtkWidget *widget, gpointer u)
-{
-	apply_new_look(FALSE);
-	gtk_main_quit();
+	switch(response) {
+		case GTK_RESPONSE_HELP:
+			show_about_dialog();
+			break;
+		case GTK_RESPONSE_APPLY:
+			apply_new_look(FALSE);
+			break;
+		case GTK_RESPONSE_OK:
+			apply_new_look(FALSE);
+		case GTK_RESPONSE_CANCEL:
+		case GTK_RESPONSE_NONE:
+			gtk_main_quit();
+	}
 }
 
 GtkWidget* create_mainwin(void)
 {
-	GtkWidget *mainwin = gtk_dialog_new();
+	GtkWidget *mainwin = gtk_dialog_new_with_buttons(
+		PROJNAME, NULL, 0,
+		GTK_STOCK_ABOUT, GTK_RESPONSE_HELP,
+		GTK_STOCK_APPLY, GTK_RESPONSE_APPLY,
+		GTK_STOCK_OK, GTK_RESPONSE_OK,
+		GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+		NULL
+	);
+
 	gtk_widget_realize(mainwin);
-	gtk_window_set_title(GTK_WINDOW(mainwin), PROJNAME);
 	gtk_window_set_resizable(GTK_WINDOW(mainwin), TRUE);
 	gtk_window_set_default_size(GTK_WINDOW(mainwin), 360, 450);
+	g_signal_connect(G_OBJECT(mainwin), "response", G_CALLBACK(response), NULL);
 	g_signal_connect(G_OBJECT(mainwin), "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
 	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(mainwin)->vbox), create_preview_pane(), FALSE, FALSE, 0);
@@ -53,32 +62,6 @@ GtkWidget* create_mainwin(void)
 	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(mainwin)->vbox), create_theme_sel(), TRUE, TRUE, 0);
 	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(mainwin)->vbox), gtk_hseparator_new(), FALSE, FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(mainwin)->vbox), create_font_sel(), FALSE, FALSE, 0);
-
-	{
-		GtkWidget* about_button = gtk_button_new_from_stock(GTK_STOCK_ABOUT);
-		g_signal_connect(G_OBJECT(about_button), "clicked", G_CALLBACK(about_clicked), NULL);
-		gtk_box_pack_end(GTK_BOX(GTK_DIALOG(mainwin)->action_area), about_button, TRUE, TRUE, 0);
-	}
-
-	{
-		GtkWidget* apply_button = gtk_button_new_from_stock(GTK_STOCK_APPLY);
-		g_signal_connect(G_OBJECT(apply_button), "clicked", G_CALLBACK(apply_clicked), NULL);
-		gtk_box_pack_start(GTK_BOX(GTK_DIALOG(mainwin)->action_area), apply_button, TRUE, TRUE, 0);
-		await_activation(apply_button);
-	}
-
-	{
-		GtkWidget* ok_button = gtk_button_new_from_stock(GTK_STOCK_OK);
-		g_signal_connect(G_OBJECT(ok_button), "clicked", G_CALLBACK(ok_clicked), NULL);
-		gtk_box_pack_start(GTK_BOX(GTK_DIALOG(mainwin)->action_area), ok_button, TRUE, TRUE, 0);
-		await_activation(ok_button);
-	}
-
-	{
-		GtkWidget *cancel_button = gtk_button_new_from_stock(GTK_STOCK_CANCEL);
-		g_signal_connect(G_OBJECT(cancel_button), "clicked", G_CALLBACK(gtk_main_quit), NULL);
-		gtk_box_pack_start(GTK_BOX(GTK_DIALOG(mainwin)->action_area), cancel_button, TRUE, TRUE, 0);
-	}
 
 	return mainwin;
 }
